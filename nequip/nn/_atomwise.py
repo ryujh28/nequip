d9f3447ba718ee480b145221a44e718b5668e878
Jae Hyun Ryu <80902490+ryujh28@users.noreply.github.com>
2023-09-05 15:14:32 +0900

    Update _atomwise.py

--- nequip/nn/_atomwise.py
+++ nequip/nn/_atomwise.py
@@ -57,6 +57,48 @@ class AtomwiseLinear(GraphModuleMixin, torch.nn.Module):
         return data
 
 
+class AtomwiseLinear_Nway(GraphModuleMixin, torch.nn.Module):
+    def __init__(
+        self,
+        field: str = AtomicDataDict.NODE_FEATURES_KEY,
+        out_field: Optional[str] = None,
+        irreps_in=None,
+        irreps_out=None,
+        N=None
+    ):
+        super().__init__()
+
+        if self.N==None:
+            self.N=1
+
+        
+        self.field = field
+        out_field = out_field if out_field is not None else field
+        self.out_field = out_field
+        if irreps_out is None:
+            irreps_out = irreps_in[field]
+
+        self._init_irreps(
+            irreps_in=irreps_in,
+            required_irreps_in=[field],
+            irreps_out={out_field: irreps_out},
+        )
+        linears = []
+        for ii in range(self.N):
+            linears.append(Linear(
+                                irreps_in=self.irreps_in[field], irreps_out=self.irreps_out[out_field]))
+        self.linears=linears
+
+    def forward(self, data: AtomicDataDict.Type) -> AtomicDataDict.Type:
+        data[self.out_field]  = self.linear(data[self.field]) * 
+        temp = [i[data[self.field] for i in self.linears]
+        temp dot ouronehot
+        return data
+
+
+
+
+
 class AtomwiseReduce(GraphModuleMixin, torch.nn.Module):
     constant: float
 
