61da763c9081b0965049c75327f5b3251e943127
Jae Hyun Ryu <80902490+ryujh28@users.noreply.github.com>
2023-09-06 13:31:10 +0900

    Update _atomwise.py

--- nequip/nn/_atomwise.py
+++ nequip/nn/_atomwise.py
@@ -222,7 +222,6 @@ class AtomwiseLinear_Nlinears(GraphModuleMixin, torch.nn.Module):
             #print(criterion_count.shape)
             #print(criterion_mix.shape)
             #print(mixture)
-            window=num_feature//6
             for atom in range(len(atom_types_TCSM)):
                 if len(mixture[atom])==0:
                     criterion_mix[atom] = 0
