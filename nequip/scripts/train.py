6701a48c43ac74f8503e42bfd39b9357d36ccb36
Jae Hyun Ryu <80902490+ryujh28@users.noreply.github.com>
2023-09-07 19:30:06 +0900

    Update train.py

--- nequip/scripts/train.py
+++ nequip/scripts/train.py
@@ -55,11 +55,23 @@ default_config = dict(
 
 
 def main(args=None, running_as_script: bool = True):
-    config = parse_command_line(args)
+    """
+    변수는 두개
+        args는 명령행 인수
+        running_as_script는 bool
+        프로그램이 스크립트로 실행되는지, 프로그램 안에서 실행되는지 확인하는 변수임
+    """
 
+    # parse_command_line 함수를 이용해서 명령행 인수를 분석함. 
+    config = parse_command_line(args)
+    
+    # 만약 running_as_script가 True라면 (default)
+    # 프로그램이 스크립트라면 logger 를 붙여줌
     if running_as_script:
+        # 
         set_up_script_logger(config.get("log", None), config.verbose)
 
+    # restart file이 있는지 확인, 파일은 있는데 config에 append하지 말라고 써있으면 에러 발생
     found_restart_file = isdir(f"{config.root}/{config.run_name}")
     if found_restart_file and not config.append:
         raise RuntimeError(
@@ -68,6 +80,7 @@ def main(args=None, running_as_script: bool = True):
         )
 
     # for fresh new train
+    # 새로운 train이라면
     if not found_restart_file:
         trainer = fresh_start(config)
     else:
@@ -121,10 +134,15 @@ def parse_command_line(args=None):
 
 def fresh_start(config):
     # we use add_to_config cause it's a fresh start and need to record it
+
+    # 버전 관리
     check_code_version(config, add_to_config=True)
+
+    # 전역 옵션 초기화 함수
     _set_global_options(config)
 
     # = Make the trainer =
+    # wandb 관련 옵션
     if config.wandb:
         import wandb  # noqa: F401
         from nequip.train.trainer_wandb import TrainerWandB
@@ -142,9 +160,13 @@ def fresh_start(config):
 
     # what is this
     # to update wandb data?
+
+    # trainer 객체의 매개변수를 설정 정보에 업데이트함 (왜?)
     config.update(trainer.params)
 
     # = Load the dataset =
+    # dataset_from_config 부터 dataset instance를 불러옴.
+    # class이름은 아마 AtomicData 일듯?
     dataset = dataset_from_config(config, prefix="dataset")
     logging.info(f"Successfully loaded the data set of type {dataset}...")
     try:
@@ -157,18 +179,22 @@ def fresh_start(config):
         validation_dataset = None
 
     # = Train/test split =
+    # trainer에 dataset을 붙여줌
     trainer.set_dataset(dataset, validation_dataset)
 
     # = Build model =
+    # 모델을 설정 정보 기반으로 빌드하고 초기화함
     final_model = model_from_config(
         config=config, initialize=True, dataset=trainer.dataset_train
     )
     logging.info("Successfully built the network...")
 
     # by doing this here we check also any keys custom builders may have added
+    # 이전 버전의 설정 키를 확인함
     _check_old_keys(config)
 
     # Equivar test
+    # Equivariance를 테스트함
     if config.equivariance_test > 0:
         n_train: int = len(trainer.dataset_train)
         assert config.equivariance_test <= n_train
@@ -190,6 +216,7 @@ def fresh_start(config):
         del errstr, indexes, n_train
 
     # Set the trainer
+    # 트레이너 객체에 최종 모델을 할당함
     trainer.model = final_model
 
     # Store any updated config information in the trainer
